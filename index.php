654686802d3b59b42cf8f679b64a5cbee0ede4dd
Chelsea Martin <chemar3412@students.ecpi.edu>
2026-04-12 21:55:17 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Welcome to PHP!</title>
+</head>
+
+<body>
+    <?php echo '<h1 style="text-align: center;">Welcome to PHP!</h1>'?>
+    <?php echo '<h2 style="text-align: center;">My Name is Chelsea Martin</h2>' ?>
+    <?php echo '<p style="text-align: center;">The Date is ' . date("Y-m-d") . '</p>' ?>
+    <?php echo '<p style="text-align: center;">The Time is ' . date("H:i:s") . '</p>' ?>
+    <?php echo '<p style="text-align: center;">This is the first performance assessment.</p>' ?>
+</body>
+
+
+
+</html>
